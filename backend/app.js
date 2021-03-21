f49da1a0f36955a6b48055e4c0404fb57d2af4d4
Frantz <frantz.neveu@sfr.fr>
2021-03-21 08:57:15 +0100

    Mise en place du CORS sur le fichier app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,16 @@
 const express = require('express');
 //appel de la methode express
 const app = express();
+
+//ajout du middleware CORS pour ajouter des headers à nos objets reponse
+//methode use pour appliquer le middleware à toute nos routes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next(); //next pour passer au middleware suivant
+  });
+
 //utilisation de la methode use pour configurer une reponse au format JSON
 app.use((req, res, next) =>{
     res.json({message: "reponse du serveur !"})
